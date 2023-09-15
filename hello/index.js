4c13b9fed38e973dafe665bd6e37b6d61a0ff689
Jørgen Thorsnes <jorgen.thorsnes@gmail.com>
2023-09-15 11:41:26 +0200

    some stuff

--- hello/index.js
+++ hello/index.js
@@ -1,13 +1,37 @@
+const { default: axios } = require("axios")
+
+
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+    if (!req.body) {
+        return { status: 400, body: 'send med en body da..' }
+    }
 
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
-    };
+    if (!req.body.method) {
+        return { status: 400, body: 'send med en body med method da' }
+    }
+
+    if (!['get', 'post', 'test'].includes(req.body.method)) {
+        return { status: 400, body: 'send med en body med method som er get eller post da' }
+    }
+
+    if (req.body.method === 'test') return { status: 200, body: "Hallo, her er jeg!" }
+
+    if (!req.body.url) {
+        return { status: 400, body: 'send med en body med url da' }
+    }
+
+    try {
+        if (req.body.method === 'post') {
+            const { data } = await axios.post(req.body.url, req.body.payload || {}, { timeout: 30000 })
+            return { status: 200, body: data }
+        } else {
+            const { data } = await axios.get(req.body.url, { timeout: 30000 })
+            return { status: 200, body: data }
+        }
+    } catch (error) {
+        const status = error.response?.status || 500
+        return { status, body: error.response?.data || error.stack || error.toString() }
+    }
 }
